2868a86076adbb3dc88250d5eb8106ed1a436c60
samkaypro <samkayode057@gmail.com>
2025-05-15 11:30:34 +0100

    fix: inventory redirection

--- app/dashboard/inventory/page.tsx
+++ app/dashboard/inventory/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import { parseCookies } from "nookies";
 import React, { useEffect } from "react";
 
 const Page = () => {
   useEffect(() => {
-    const cookies = parseCookies();
-    const Token = cookies.authToken;
-    // console.log(Token);
-    if (Token) {
-      window.location.href = `https://erp.vendcliq.com/main?page=Dashboard&email=${cookies.email}`;
-    }
+    window.location.href = "https://erp.vendcliq.com/";
   }, []);
+
   return (
     <div>
       <p className="text-2xl p-10">Redirecting to Inventory...</p>
