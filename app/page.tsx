d9470af11334899b9b02832fc07d45c65016fa3f
opeyemi-2018 <opeyemiodunayo2018@gmail.com>
2025-12-19 23:07:52 +0100

    change

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/signup");
+    router.push("/signin");
   });
   return <div>{/* <Login /> */}</div>;
 }
